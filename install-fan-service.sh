df6921d102bde0938597281b85539720d07cb5ed
René Fa <farene1996+github@gmail.com>
2025-05-30 16:41:42 +0200

    refactor: Auto format files

--- install-fan-service.sh
+++ install-fan-service.sh
@@ -2,11 +2,11 @@
 
 echo "Start installing fan service..."
 
-sudo cp -f ./x-c1-fan.sh                /usr/local/bin/
+sudo cp -f ./x-c1-fan.sh /usr/local/bin/
 sudo chmod +x /usr/local/bin/x-c1-fan.sh
-sudo cp -f ./x-c1-fan.service           /lib/systemd/system
+sudo cp -f ./x-c1-fan.service /lib/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable x-c1-fan
-sudo systemctl start x-c1-fan
+sudo systemctl restart x-c1-fan
 
 echo "Fan service installed"
--- xPWR.sh
+++ xPWR.sh
@@ -1,56 +1,59 @@
-#!/bin/bash  
+#!/bin/bash
 
 #  Use gpiod instead of obsolete interface, and suuports ubuntu 23.04 also
 
-# Make sure enough parameters are passed in  
-if [ "$#" -ne 3 ]; then  
-  echo "Usage: $0 <pwm_chip> <shutdown_pin> <boot_pin>"  
-  exit 1  
-fi  
+# Make sure enough parameters are passed in
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <pwm_chip> <shutdown_pin> <boot_pin>"
+  exit 1
+fi
 
-GPIOCHIP=$1    
-SHUTDOWN=$2  
+GPIOCHIP=$1
+SHUTDOWN=$2
 BOOT=$3
 
 # Checks if the passed parameter is an integer
 re='^[0-9\.]+$'
-if ! [[ $GPIOCHIP =~ $re ]] ; then
-   echo "error: gpio_chip is not a number" >&2; exit 1
+if ! [[ $GPIOCHIP =~ $re ]]; then
+  echo "error: gpio_chip is not a number" >&2
+  exit 1
 fi
 
-if ! [[ $SHUTDOWN =~ $re ]] ; then
-   echo "error: shutdown_pin is not a number" >&2; exit 1
+if ! [[ $SHUTDOWN =~ $re ]]; then
+  echo "error: shutdown_pin is not a number" >&2
+  exit 1
 fi
 
-if ! [[ $BOOT =~ $re ]] ; then
-   echo "error: button_pin is not a number" >&2; exit 1
+if ! [[ $BOOT =~ $re ]]; then
+  echo "error: button_pin is not a number" >&2
+  exit 1
 fi
 
-REBOOTPULSEMINIMUM=200  
-REBOOTPULSEMAXIMUM=600  
-  
+REBOOTPULSEMINIMUM=200
+REBOOTPULSEMAXIMUM=600
+
 # Initialize the BOOT pin to 1
-gpioset $GPIOCHIP $BOOT=1  
-  
-while [ 1 ]; do  
-  shutdownSignal=$(gpioget $GPIOCHIP $SHUTDOWN)  
-  if [ $shutdownSignal -eq 0 ]; then  
-    sleep 0.2  
-  else  
-    pulseStart=$(date +%s%N | cut -b1-13)  
-    while [ $shutdownSignal -eq 1 ]; do  
-      sleep 0.02  
-      if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMAXIMUM ]; then  
-        echo "Your device is shutting down on pin $SHUTDOWN, halting Rpi ..."  
-        sudo poweroff  
-        exit  
-      fi  
-      shutdownSignal=$(gpioget $GPIOCHIP $SHUTDOWN)  
-    done  
-    if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMINIMUM ]; then  
-      echo "Your device is rebooting on pin $SHUTDOWN, recycling Rpi ..."  
-      sudo reboot  
-      exit  
-    fi  
-  fi  
+gpioset $GPIOCHIP $BOOT=1
+
+while [ 1 ]; do
+  shutdownSignal=$(gpioget $GPIOCHIP $SHUTDOWN)
+  if [ $shutdownSignal -eq 0 ]; then
+    sleep 0.2
+  else
+    pulseStart=$(date +%s%N | cut -b1-13)
+    while [ $shutdownSignal -eq 1 ]; do
+      sleep 0.02
+      if [ $(($(date +%s%N | cut -b1-13) - $pulseStart)) -gt $REBOOTPULSEMAXIMUM ]; then
+        echo "Your device is shutting down on pin $SHUTDOWN, halting Rpi ..."
+        sudo poweroff
+        exit
+      fi
+      shutdownSignal=$(gpioget $GPIOCHIP $SHUTDOWN)
+    done
+    if [ $(($(date +%s%N | cut -b1-13) - $pulseStart)) -gt $REBOOTPULSEMINIMUM ]; then
+      echo "Your device is rebooting on pin $SHUTDOWN, recycling Rpi ..."
+      sudo reboot
+      exit
+    fi
+  fi
 done
