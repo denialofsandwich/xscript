4edacf9830d3812350b19ed986527a2e9b709f20
Rene Fa <farene1996@gmail.com>
2026-03-23 11:51:16 +0100

    fix: Use new syntax of libgpiod

--- xPWR.sh
+++ xPWR.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#  Use gpiod instead of obsolete interface, and suuports ubuntu 23.04 also
-
 # Make sure enough parameters are passed in
 if [ "$#" -ne 3 ]; then
   echo "Usage: $0 <pwm_chip> <shutdown_pin> <boot_pin>"
@@ -12,20 +10,21 @@ GPIOCHIP=$1
 SHUTDOWN=$2
 BOOT=$3
 
-# Checks if the passed parameter is an integer
-re='^[0-9\.]+$'
+# Checks if the passed parameters got the correct format
+re='^\/dev\/gpiochip[0-9\.]+$'
 if ! [[ $GPIOCHIP =~ $re ]]; then
-  echo "error: gpio_chip is not a number" >&2
+  echo "error: gpio_chip is not a path" >&2
   exit 1
 fi
 
+re='^GPIO[0-9\.]+$'
 if ! [[ $SHUTDOWN =~ $re ]]; then
-  echo "error: shutdown_pin is not a number" >&2
+  echo "error: shutdown_pin is invalid" >&2
   exit 1
 fi
 
 if ! [[ $BOOT =~ $re ]]; then
-  echo "error: button_pin is not a number" >&2
+  echo "error: button_pin is invalid" >&2
   exit 1
 fi
 
@@ -33,22 +32,24 @@ REBOOTPULSEMINIMUM=200
 REBOOTPULSEMAXIMUM=600
 
 # Initialize the BOOT pin to 1
-gpioset $GPIOCHIP $BOOT=1
+gpioset -c "$GPIOCHIP" -p 1s -t 0 "$BOOT=1"
 
-while [ 1 ]; do
-  shutdownSignal=$(gpioget $GPIOCHIP $SHUTDOWN)
-  if [ $shutdownSignal -eq 0 ]; then
+echo "Successfully started"
+
+while true; do
+  shutdownSignal=$(gpioget -c "$GPIOCHIP" --numeric "$SHUTDOWN")
+  if [ "$shutdownSignal" -eq 0 ]; then
     sleep 0.2
   else
     pulseStart=$(date +%s%N | cut -b1-13)
-    while [ $shutdownSignal -eq 1 ]; do
+    while [ "$shutdownSignal" -eq 1 ]; do
       sleep 0.02
       if [ $(($(date +%s%N | cut -b1-13) - $pulseStart)) -gt $REBOOTPULSEMAXIMUM ]; then
         echo "Your device is shutting down on pin $SHUTDOWN, halting Rpi ..."
         sudo poweroff
         exit
       fi
-      shutdownSignal=$(gpioget $GPIOCHIP $SHUTDOWN)
+      shutdownSignal=$(gpioget -c "$GPIOCHIP" --numeric "$SHUTDOWN")
     done
     if [ $(($(date +%s%N | cut -b1-13) - $pulseStart)) -gt $REBOOTPULSEMINIMUM ]; then
       echo "Your device is rebooting on pin $SHUTDOWN, recycling Rpi ..."
--- xSoft.sh
+++ xSoft.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
 
-#  Use gpiod instead of obsolete interface, and suuports ubuntu 23.04 also
-
-# 检查是否提供了足够的命令行参数
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <pwm_chip> <button_pin>" >&2
-    exit 1
+  echo "Usage: $0 <pwm_chip> <button_pin>" >&2
+  exit 1
 fi
 
-PWMCHIP=$1
+GPIOCHIP=$1
 BUTTON=$2
 
-SLEEP=2
-
-# 检查传入的参数是否为整数
-
-re='^[0-9\.]+$'
-if ! [[ $PWMCHIP =~ $re ]] ; then
-   echo "error: pwm_chip is not a number" >&2; exit 1
+re='^\/dev\/gpiochip[0-9\.]+$'
+if ! [[ $GPIOCHIP =~ $re ]]; then
+  echo "error: gpio_chip is not a path" >&2
+  exit 1
 fi
 
-if ! [[ $BUTTON =~ $re ]] ; then
-   echo "error: button_pin is not a number" >&2; exit 1
+re='^GPIO[0-9\.]+$'
+if ! [[ $BUTTON =~ $re ]]; then
+  echo "error: button_pin is invalid" >&2
+  exit 1
 fi
 
-echo "Your device will be shutting down in $SLEEP seconds..."
-
-gpioset $PWMCHIP $BUTTON=1
-
-sleep $SLEEP
-
-# Restore GPIO
-# This step is necessary, otherwise you will have to press the onboard button twice to turn on the device, and the same applies to the AUTO ON function.
-gpioset $PWMCHIP $BUTTON=0
+echo "Triggering shutdown..."
+gpioset -c "$GPIOCHIP" -p 2s -t 0 "$BUTTON=1"
+gpioset -c "$GPIOCHIP" -t 0 "$BUTTON=0"
